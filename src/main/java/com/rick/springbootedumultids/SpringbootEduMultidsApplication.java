21c0c6d5e9316235d5b5c970badc5ae9553cd450
zrbfree@163.com <zrb117>
2017-08-25 18:01:10 +0800

    spring boot 多数据源

---
+++ src/main/java/com/rick/springbootedumultids/SpringbootEduMultidsApplication.java
@@ -0,0 +1,12 @@
+package com.rick.springbootedumultids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEduMultidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEduMultidsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rick/springbootedumultids/SpringbootEduMultidsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rick.springbootedumultids;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootEduMultidsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
